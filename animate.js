ea326230f5301257e0da0f3a1ef9ed4411ded7b0
Emily Amspoker <eamspoker21@kentdenver.org>
2021-01-26 07:44:13 -0700

    create basic diffusion

--- animate.js
+++ animate.js
@@ -1,4 +1,4 @@
-let arr = Array(15).fill().map(() => Array(15).fill(0));
+var arr_global = Array(15).fill().map(() => Array(15).fill(0));
 let dt = 0;
 function setup() {
     createCanvas(800, 800);
@@ -8,13 +8,15 @@ function setup() {
 function draw() {
     clear();
 
-    update_sources(arr[7][7]);
-    //diffuse(); 
+    update_sources(arr_global[7][7]);
+
+    dt=millis()/10000;
+    diffuse(arr_global); 
 
     for(var i = 0; i < 15; i++){
         for(var j = 0; j < 15; j++){
             squareColor = color(100, 50, 100);
-            squareColor.setAlpha(arr[i][j]);
+            squareColor.setAlpha(arr_global[i][j]);
             fill(squareColor);
             square((j*50)+25, (i*50)+25, 50);
         }
@@ -22,21 +24,22 @@ function draw() {
 }
 
 function update_sources(currDens){
-    arr[7][7] = currDens + .3;
+    arr_global[7][7] = currDens + 10000;
 }
 
-function diffuse(){
+function diffuse(arr){
     let arr0 = [...arr];
     let diff = .1;
-    let a = diff*15*15;
+    let a = diff*15*15*dt;
     for(var k = 0; k < 20; k++){
-        for(var i = 1; i < 15; i++){
-            for(var j = 1; j < 15; j++){
-                arr[i][j] = (arr0[i,j] + a*(arr[i-1,j]+arr[i+1,j]+
-                arr[i,j-1]+arr[i,j+1]))/(1+4*a);
+        for(var i = 1; i < 14; i++){
+            for(var j = 1; j < 14; j++){
+                arr[i][j] = (arr0[i][j] + a*(arr[i-1][j]+arr[i+1][j]+
+                arr[i][j-1]+arr[i][j+1]))/(1+4*a);
             }
         }
     }
-    console.log(arr);
+
+    
 }
 
